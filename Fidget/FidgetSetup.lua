5b142f2d0ba916825dc3d2893db744f59a716d74
SOOMUCHLAG <rafitrzebuchow@gmail.com>
2025-12-31 18:51:06 +0100

    Add files via upload

---
+++ Fidget/FidgetSetup.lua
@@ -0,0 +1,9 @@
+FidgetMetatables = {}
+
+
+Fidget = {}
+Fidget.rigidbodies, Fidget.particles  = require("Fidget.Rigidbody Init.Rigidbody")
+Fidget.physicsSim = require("Fidget.Physics Simulation Vars.Physics Simulation")
+Fidget.joints = require("Fidget.Rigidbody Init.Joints")
+Fidget.quaternions = require("Fidget.quaternions")
+return Fidget
