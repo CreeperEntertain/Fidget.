a0a3873b7ad4d72d7248cc9e7f349dbf3d0bfbf1
SOOMUCHLAG <rafitrzebuchow@gmail.com>
2026-01-21 23:25:42 +0100

    Delete Fidget directory

--- Fidget/FidgetSetup.lua
+++
@@ -1,9 +0,0 @@
-FidgetMetatables = {}
-
-
-Fidget = {}
-Fidget.rigidbodies, Fidget.particles  = require("Fidget.Rigidbody Init.Rigidbody")
-Fidget.physicsSim = require("Fidget.Physics Simulation Vars.Physics Simulation")
-Fidget.joints = require("Fidget.Rigidbody Init.Joints")
-Fidget.quaternions = require("Fidget.quaternions")
-return Fidget
